fc83d65131320a03e139a04f15855e9e0c60f025
Jubayer Hossain <jubayerhossain9999@gmail.com>
2025-07-13 18:06:29 +0600

    Add dotenv support and refactor wallet.js for environment variable usage

--- wallet.js
+++ wallet.js
@@ -1,31 +1,35 @@
+/* eslint-disable no-undef */
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import "dotenv/config"; // Load environment variables from .env file
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const acapyApiUrl = process.env.ACAPY_API_URL;
+const acapyAdminKey = process.env.ACAPY_ADMIN_KEY; 
 
 app.use(cors());
 app.use(express.json());
 
 app.post("/create-tenant", async (req, res) => {
-  const { wallet_label, wallet_name, roles } = req.body; 
+  const { wallet_label, wallet_name, roles } = req.body;
 
   const requestData = {
-    wallet_label: wallet_label, 
-    wallet_name: wallet_name, 
-    roles: roles, 
+    wallet_label: wallet_label,
+    wallet_name: wallet_name,
+    roles: roles,
   };
 
   try {
     const response = await axios.post(
-      "https://221257d0ccd9.ngrok-free.app/tenant-admin/v1/tenants",
+      `${acapyApiUrl}/tenant-admin/v1/tenants`, 
       requestData,
       {
         headers: {
           accept: "application/json",
           "Content-Type": "application/json",
-          "x-api-key": "tenant-admin.adminApiKey",
+          "x-api-key": acapyAdminKey,
         },
       }
     );
@@ -33,8 +37,8 @@ app.post("/create-tenant", async (req, res) => {
     // Respond with the data from the API
     res.json({
       success: true,
-      wallet_id: response.data.wallet_id, 
-      access_token: response.data.access_token, 
+      wallet_id: response.data.wallet_id,
+      access_token: response.data.access_token,
     });
   } catch (error) {
     console.error("Error:", error);
